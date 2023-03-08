e8d817895b498e19120839c3133f94dd2194ffb4
Sinehan Ezhilmuthu <sezhilmuthu@ucmerced.edu>
2023-03-08 23:42:13 +0000

    Bootstrapped project & created display menu

---
+++ exercise-1/wordle/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    cout << "=========================" << endl;
+    cout << "WELCOME TO WORDLE" << endl; 
+    cout << "=========================" << endl;
+    cout << "1. Play Wordle" << endl;
+    cout << "2. How to Play" << endl;
+    cout << "3. Statistics Summary" << endl;
+    cout << "4. Reset Statistics" << endl;
+    cout << "5. Exit\n" << endl; 
+    cout << "Select an option: "; 
+
+    
+    int option;
+    cin >> option;
+
+    while (option != 5) {
+
+    }
+    cout << "Bye" << endl;
+
+    return 0;
+}
