4181f2b6f7fa1bb4158ec8584c6cd639e486d929
Sinehan Ezhilmuthu <sezhilmuthu@ucmerced.edu>
2023-03-17 01:20:45 +0000

    Set up menu and how to play  as function

--- exercise-1/wordle/main.cpp
+++ exercise-1/wordle/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
+
+#define GREEN "\033[32m"
+#define YELLOW "\033[93m"
+#define GREY "\033[90m"
+#define RESET "\033[0m"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-
+void displayMenu() {
     cout << "=========================" << endl;
     cout << "WELCOME TO WORDLE" << endl; 
     cout << "=========================" << endl;
@@ -13,15 +18,49 @@ int main(int argc, char* argv[]) {
     cout << "4. Reset Statistics" << endl;
     cout << "5. Exit\n" << endl; 
     cout << "Select an option: "; 
+}
 
-    
-    int option;
-    cin >> option;
+void displayHowToPlay() {
+    cout << "=============================================" << endl;
+    cout << "                 HOW TO PLAY                 " << endl;
+    cout << "=============================================" << endl;            
+    cout << "Guess the Wordle in 6 tries.\n\n" << endl;
+    cout << "HOW TO PLAY:" << endl;
+    cout << "- Each guess must be a valid 5 letter word." << endl;
+    cout << "- The color of the tiles will change to show\n  you how close your guess was to the word.\n" << endl;
+    cout << GREEN << " --- " << RESET << " ---  ---  ---  --- " << endl;
+    cout << GREEN <<"| W |"<< RESET << "| E || A || R || Y |" << endl;
+    cout << GREEN << " --- " << RESET << " ---  ---  ---  --- " << endl;
+    cout << "W is in the word and in the correct spot.\n" << endl;
+    cout << " --- " << YELLOW << " --- " << RESET << " ---  ---  --- " << endl;
+    cout << "| P |" << YELLOW << "| I |" << RESET << "| L || L || S |" << endl;
+    cout << " --- " << YELLOW << " --- " << RESET << " ---  ---  --- " << endl;
+    cout << "I is in the word but in the wrong spot.\n" << endl;
+    cout << " ---  ---  --- " << GREY << " --- " << RESET << " --- " << endl;
+    cout << "| V || A || G |" << GREY << "| U |" << RESET << "| E |" << endl;
+    cout << " ---  ---  --- " << GREY << " --- " << RESET << " --- " << endl;
+    cout << "U is not in the word in any spot.\n\n" << endl;
+    cout << "Press [enter] to continue";
+    cin.ignore();
+}
 
-    while (option != 5) {
+int main() {
 
+    string option;
+    while (option != "5") {
+        displayMenu();
+        getline(cin, option);
+        if (option == "1") {
+            cout << "Wordle game not implemented yet.\n" << endl;
+        } else if (option == "2") {
+            displayHowToPlay();
+        } else if (option == "3") {
+            cout << "Statistics summary not implemented yet.\n" << endl;
+        } else if (option == "4") {
+            cout << "Reset statistics not implemented yet.\n" << endl;
+        } else {
+            cout << "Bye" << endl;
+        }
     }
-    cout << "Bye" << endl;
-
     return 0;
 }
