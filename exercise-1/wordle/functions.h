321278dd85c32af04c5d1071a43050a2ce6ab869
Sinehan Ezhilmuthu <sezhilmuthu@ucmerced.edu>
2023-03-18 19:06:46 +0000

    completed implementation of wordle game

---
+++ exercise-1/wordle/functions.h
@@ -0,0 +1,217 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#define GREEN "\033[32m"
+#define YELLOW "\033[93m"
+#define GREY "\033[90m"
+#define RESET "\033[0m"
+
+using namespace std;
+
+void displayMenu() {
+    cout << "=========================" << endl;
+    cout << "WELCOME TO WORDLE" << endl; 
+    cout << "=========================" << endl;
+    cout << "1. Play Wordle" << endl;
+    cout << "2. How to Play" << endl;
+    cout << "3. Statistics Summary" << endl;
+    cout << "4. Reset Statistics" << endl;
+    cout << "5. Exit\n" << endl; 
+    cout << "Select an option: "; 
+}
+
+
+void displayHowToPlay() {
+    system("clear");
+    cout << "=============================================" << endl;
+    cout << "                 HOW TO PLAY                 " << endl;
+    cout << "=============================================" << endl;            
+    cout << "Guess the Wordle in 6 tries.\n\n" << endl;
+    cout << "HOW TO PLAY:" << endl;
+    cout << "- Each guess must be a valid 5 letter word." << endl;
+    cout << "- The color of the tiles will change to show\n  you how close your guess was to the word.\n" << endl;
+    cout << GREEN << " --- " << RESET << " ---  ---  ---  --- " << endl;
+    cout << GREEN <<"| W |"<< RESET << "| E || A || R || Y |" << endl;
+    cout << GREEN << " --- " << RESET << " ---  ---  ---  --- " << endl;
+    cout << "W is in the word and in the correct spot.\n" << endl;
+    cout << " --- " << YELLOW << " --- " << RESET << " ---  ---  --- " << endl;
+    cout << "| P |" << YELLOW << "| I |" << RESET << "| L || L || S |" << endl;
+    cout << " --- " << YELLOW << " --- " << RESET << " ---  ---  --- " << endl;
+    cout << "I is in the word but in the wrong spot.\n" << endl;
+    cout << " ---  ---  --- " << GREY << " --- " << RESET << " --- " << endl;
+    cout << "| V || A || G |" << GREY << "| U |" << RESET << "| E |" << endl;
+    cout << " ---  ---  --- " << GREY << " --- " << RESET << " --- " << endl;
+    cout << "U is not in the word in any spot.\n\n" << endl;
+    cout << "Press [enter] to continue";
+    cin.ignore();
+    system("clear");
+}
+
+string randomWord() {
+    vector<string> words;
+    string word;
+    ifstream file("words.txt");
+    if (file.is_open()) {
+        while (getline(file, word)) {
+            words.push_back(word);
+        }
+        file.close();
+    }
+    srand(time(0));
+    int randomIndex = rand() % words.size();
+    return words[randomIndex];
+}
+
+bool guessCheck(string word) {
+    ifstream file1("words.txt");
+    if (file1.is_open()) {
+        string w;
+        while (getline(file1, w)) {
+            if (w == word) {
+                file1.close();
+                return true;
+            }
+        }
+        file1.close();
+    }
+    ifstream file2("allowed.txt");
+    if (file2.is_open()) {
+        string w;
+        while (getline(file2, w)) {
+            if (w == word) {
+                file2.close();
+                return true;
+            }
+        }
+        file2.close();
+    }
+    return false;
+}
+
+void wordleMain() {
+    system("clear");
+    string answer = randomWord();
+
+    string guess;
+    while(guess.length() != 5) {
+        cout << "Enter Your Guess: ";
+        cin >> guess;
+        system("clear");
+    }
+
+    vector<char> vGuess;
+    vector<char> vAnswer;
+    vector<string> colors = {GREY, GREY, GREY, GREY, GREY};
+
+    vector<vector<char>> guesses;
+    vector<vector<string>> vColors;
+
+    for (int i = 0; i < answer.length(); i++) {
+            vAnswer.push_back(answer[i]);
+    }
+
+    int guessCount = 0;
+    while (guessCount != 5 && guess != answer) {
+        system("clear");
+        for (int i = 0; i < answer.length(); i++) {
+                vGuess.push_back(guess[i]);
+        }
+
+        guesses.push_back(vGuess);
+
+        for (int g = 0; g < vAnswer.size(); g++) {
+            if (vGuess[g] == vAnswer[g]) {
+                colors[g] = GREEN;
+            }
+        }
+
+        for (int y = 0; y < vAnswer.size(); y++) {
+            for (int x = 0; x < vAnswer.size(); x++) {
+                if (x != y && vGuess[y]==vAnswer[x]) {
+                    colors[y] = YELLOW;
+                }
+            }
+        }
+        vColors.push_back(colors);
+        for (int i = 0; i < guesses.size(); i++) {
+            cout << vColors[i][0] << " --- " << vColors[i][1] << " --- " << vColors[i][2] << " --- " << vColors[i][3] << " --- " << vColors[i][4] << " --- " << RESET << endl;
+            cout << vColors[i][0] << "| " << static_cast<char>(toupper(guesses[i][0])) << " |" << vColors[i][1] << "| " << static_cast<char>(toupper(guesses[i][1])) << " |" << vColors[i][2] << "| " << static_cast<char>(toupper(guesses[i][2])) << " |" << vColors[i][3] << "| " << static_cast<char>(toupper(guesses[i][3])) << " |" << vColors[i][4] << "| " << static_cast<char>(toupper(guesses[i][4])) << " |" << RESET << endl;
+            cout << vColors[i][0] << " --- " << vColors[i][1] << " --- " << vColors[i][2] << " --- " << vColors[i][3] << " --- " << vColors[i][4] << " --- " << RESET << endl;
+        }   
+        // cout << colors[0] << " --- " << colors[1] << " --- " << colors[2] << " --- " << colors[3] << " --- " << colors[4] << " --- " << RESET << endl;
+        // cout << colors[0] << "| " << static_cast<char>(toupper(vGuess[0])) << " |" << colors[1] << "| " << static_cast<char>(toupper(vGuess[1])) << " |" << colors[2] << "| " << static_cast<char>(toupper(vGuess[2])) << " |" << colors[3] << "| " << static_cast<char>(toupper(vGuess[3])) << " |" << colors[4] << "| " << static_cast<char>(toupper(vGuess[4])) << " |" << RESET << endl;
+        // cout << colors[0] << " --- " << colors[1] << " --- " << colors[2] << " --- " << colors[3] << " --- " << colors[4] << " --- " << RESET << endl;
+        guessCount++;
+
+        guess = "";
+
+    while(guess.length() != 5) {
+        cout << "Enter Your Guess: ";
+        cin >> guess;
+        if (!guessCheck(guess)) {
+            guess = "";
+        }
+    }
+
+
+        vGuess.clear();
+
+        colors = {GREY, GREY, GREY, GREY, GREY};
+    }
+    
+   for (int i = 0; i < answer.length(); i++) {
+            vGuess.push_back(guess[i]);
+    }
+
+    for (int g = 0; g < vAnswer.size(); g++) {
+        if (vGuess[g] == vAnswer[g]) {
+            colors[g] = GREEN;
+        }
+    }
+
+    for (int y = 0; y < vAnswer.size(); y++) {
+        for (int x = 0; x < vAnswer.size(); x++) {
+            if (x != y && vGuess[y]==vAnswer[x]) {
+                colors[y] = YELLOW;
+            }
+        }
+    }
+    guesses.push_back(vGuess);
+    vColors.push_back(colors);
+    system("clear");
+    for (int i = 0; i < guesses.size(); i++) {
+        cout << vColors[i][0] << " --- " << vColors[i][1] << " --- " << vColors[i][2] << " --- " << vColors[i][3] << " --- " << vColors[i][4] << " --- " << RESET << endl;
+        cout << vColors[i][0] << "| " << static_cast<char>(toupper(guesses[i][0])) << " |" << vColors[i][1] << "| " << static_cast<char>(toupper(guesses[i][1])) << " |" << vColors[i][2] << "| " << static_cast<char>(toupper(guesses[i][2])) << " |" << vColors[i][3] << "| " << static_cast<char>(toupper(guesses[i][3])) << " |" << vColors[i][4] << "| " << static_cast<char>(toupper(guesses[i][4])) << " |" << RESET << endl;
+        cout << vColors[i][0] << " --- " << vColors[i][1] << " --- " << vColors[i][2] << " --- " << vColors[i][3] << " --- " << vColors[i][4] << " --- " << RESET << endl;
+    }
+    // cout << vColors[guesses.size()-1][0] << " --- " << vColors[guesses.size()-1][1] << " --- " << vColors[guesses.size()-1][2] << " --- " << vColors[guesses.size()-1][3] << " --- " << vColors[guesses.size()-1][4] << " --- " << RESET << endl;
+    // cout << vColors[guesses.size()-1][0] << "| " << static_cast<char>(toupper(guesses[guesses.size()-1][0])) << " |" << vColors[guesses.size()-1][1] << "| " << static_cast<char>(toupper(guesses[guesses.size()-1][1])) << " |" << vColors[guesses.size()-1][2] << "| " << static_cast<char>(toupper(guesses[guesses.size()-1][2])) << " |" << vColors[guesses.size()-1][3] << "| " << static_cast<char>(toupper(guesses[guesses.size()-1][3])) << " |" << vColors[guesses.size()-1][4] << "| " << static_cast<char>(toupper(guesses[guesses.size()-1][4])) << " |" << RESET << endl;
+    // cout << vColors[guesses.size()-1][0] << " --- " << vColors[guesses.size()-1][1] << " --- " << vColors[guesses.size()-1][2] << " --- " << vColors[guesses.size()-1][3] << " --- " << vColors[guesses.size()-1][4] << " --- " << RESET << endl;
+    
+    if (guess == answer) {
+        cout << "\nWinner Winner Chicken Dinner!\n\n" << endl;
+    } else {
+        string print;
+        for (int i = 0; i < answer.length(); i++) {
+            print += toupper(answer[i]);
+        }
+        cout << "\n\nThe word was: " << GREEN << print << RESET << endl;
+        cout << "\nYou Did Great, Try again later!!\n\n" << endl;
+    }
+
+    cout << "Press [enter] to continue";
+    cin.ignore();
+    string dummy;
+    getline(cin, dummy);
+    system("clear");
+}
+
+#endif
--- exercise-1/wordle/main.cpp
+++ exercise-1/wordle/main.cpp
@@ -1,57 +1,19 @@
 #include <iostream>
 #include <string>
-
-#define GREEN "\033[32m"
-#define YELLOW "\033[93m"
-#define GREY "\033[90m"
-#define RESET "\033[0m"
+#include "functions.h"
 
 using namespace std;
 
-void displayMenu() {
-    cout << "=========================" << endl;
-    cout << "WELCOME TO WORDLE" << endl; 
-    cout << "=========================" << endl;
-    cout << "1. Play Wordle" << endl;
-    cout << "2. How to Play" << endl;
-    cout << "3. Statistics Summary" << endl;
-    cout << "4. Reset Statistics" << endl;
-    cout << "5. Exit\n" << endl; 
-    cout << "Select an option: "; 
-}
 
-void displayHowToPlay() {
-    cout << "=============================================" << endl;
-    cout << "                 HOW TO PLAY                 " << endl;
-    cout << "=============================================" << endl;            
-    cout << "Guess the Wordle in 6 tries.\n\n" << endl;
-    cout << "HOW TO PLAY:" << endl;
-    cout << "- Each guess must be a valid 5 letter word." << endl;
-    cout << "- The color of the tiles will change to show\n  you how close your guess was to the word.\n" << endl;
-    cout << GREEN << " --- " << RESET << " ---  ---  ---  --- " << endl;
-    cout << GREEN <<"| W |"<< RESET << "| E || A || R || Y |" << endl;
-    cout << GREEN << " --- " << RESET << " ---  ---  ---  --- " << endl;
-    cout << "W is in the word and in the correct spot.\n" << endl;
-    cout << " --- " << YELLOW << " --- " << RESET << " ---  ---  --- " << endl;
-    cout << "| P |" << YELLOW << "| I |" << RESET << "| L || L || S |" << endl;
-    cout << " --- " << YELLOW << " --- " << RESET << " ---  ---  --- " << endl;
-    cout << "I is in the word but in the wrong spot.\n" << endl;
-    cout << " ---  ---  --- " << GREY << " --- " << RESET << " --- " << endl;
-    cout << "| V || A || G |" << GREY << "| U |" << RESET << "| E |" << endl;
-    cout << " ---  ---  --- " << GREY << " --- " << RESET << " --- " << endl;
-    cout << "U is not in the word in any spot.\n\n" << endl;
-    cout << "Press [enter] to continue";
-    cin.ignore();
-}
 
 int main() {
-
+    system("clear");
     string option;
     while (option != "5") {
         displayMenu();
         getline(cin, option);
         if (option == "1") {
-            cout << "Wordle game not implemented yet.\n" << endl;
+            wordleMain();
         } else if (option == "2") {
             displayHowToPlay();
         } else if (option == "3") {
@@ -64,3 +26,4 @@ int main() {
     }
     return 0;
 }
+
